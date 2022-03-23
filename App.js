99bd53600b2917e6f9ec638a879fe5e221a4875c
meesh-hwang <meesh.hwang@gmail.com>
2022-03-23 15:50:31 -0700

    a

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 
 import Home from './pages/Home';
 import NotFound from "./pages/NotFound";
@@ -12,6 +12,7 @@ import Header from './components/Header';
 
 const App = () => {
   return (
+    <HashRouter>
       <Router>
         <Header />
         <Routes>
@@ -21,6 +22,7 @@ const App = () => {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
+    </HashRouter>
   );
 }
 
