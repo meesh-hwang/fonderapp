6995168070bee19d1759cc167f0950d1f083ac4d
meesh-hwang <meesh.hwang@gmail.com>
2022-03-23 16:03:18 -0700

    a

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,12 @@ export default function Header(active){
         <header>
             <div>
             <img src="../assets/icon.png"/>
-            <NavLink to="/"><h1>Fonder</h1></NavLink>
+            <NavLink to="/fonderapp"><h1>Fonder</h1></NavLink>
             </div>
             <nav>
                 <ul>
                     <li><NavLink to="/fonderapp/support">Support</NavLink></li>
-                    <li><NavLink to="submission">Submission</NavLink></li>
+                    <li><NavLink to="/fonderapp/submission">Submission</NavLink></li>
                 </ul>
             </nav>
         </header>
