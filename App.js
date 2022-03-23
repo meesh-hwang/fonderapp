6dd3c72c8a5449ab2ade385c415a7cacc3845c0d
meesh-hwang <meesh.hwang@gmail.com>
2022-03-23 16:00:47 -0700

    a

--- App.js
+++ App.js
@@ -16,8 +16,8 @@ const App = () => {
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/fonderapp/support" element={<Support />} />
-          <Route path="/fonderapp/submission" element={<Submission />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/submission" element={<Submission />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
