843cc21be71c9b0723912ca66f1e712ae7385440
meesh-hwang <meesh.hwang@gmail.com>
2022-03-23 15:36:37 -0700

    a

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Router, Routes, Route } from "react-router-dom";
 
 import Home from './pages/Home';
 import NotFound from "./pages/NotFound";
