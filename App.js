b0f7fff29eeae7284be083a045650911b7c63eef
Edward Lin <46078915+EdwardOfLinster@users.noreply.github.com>
2022-03-29 14:57:14 -0700

    Update App.js

--- App.js
+++ App.js
@@ -20,9 +20,9 @@ const App = () => {
                 <title>Fonder</title>
         </Helmet>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/support" element={<Support />} />
-          <Route path="/submission" element={<Submission/>} />
+          <Route path="/fonderapp/" element={<Home />} />
+          <Route path="/fonderapp/support" element={<Support />} />
+          <Route path="/fonderapp/submission" element={<Submission/>} />
         </Routes>
         <Footer />
       </BrowserRouter>
